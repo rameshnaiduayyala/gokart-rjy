d5ed2120bf4ad1735a9ad88aa00575902a403476
Karan bohra <its.karanbohra556@gmail.com>
2024-11-25 18:49:05 +0530

    Basic Configs and tailwind setups

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <section className='relative h-dvh w-screen overflow-x-hidden'>
+            hello
+        </section>
+    )
+}
+
+export default HeroSection
